91c4e50730a1fb3443525364a032a7ca40c027a6
oivchenko <oivchenko@gmail.com>
2022-02-14 18:10:04 +0200

    gggggg

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
 
+
   eleventyConfig.addFilter('readableDate', dateObj => {
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
       'dd LLL yyyy'
